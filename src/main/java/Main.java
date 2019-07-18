9ed6d57b39bfc24d3f7e01015872ea8d816c17b4
Facundo Sanchez <fas_facu@hotmail.com>
2019-07-18 00:17:04 -0300

    pasando todo a ingles y arreglando los estilos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,10 @@
 public class Main {
 
+  public static void main(String[] args) {
+    System.out.println("Hello Worl");
+  }
 
-    //recordatorio: usar el psvm y luego tab
-    public static void main(String[] args) {
-        System.out.println("Hello Worl");
-    }
-
-    public static int suma(int a, int b){
-        return  a + b;
-    }
+  public static int suma(int a, int b){
+    return  a + b;
+  }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,19 +1,14 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-
-
 
 public class MainTest {
 
-
-    @Test
-    public void sumar()
-    {
-       int resultado = Main.suma(1,2);
-       int esperado = 3;
-
-       assertEquals(esperado,resultado);
-    }
+  @Test
+  public void sumTest()
+  {
+    int result = Main.suma(1,2);
+    int expectedResult = 3;
+    assertEquals(expectedResult,result);
+  }
 }
